deaafd10cb08187afbcf2250cedfa23e582b0499
valeria <valerie.cardoso@gmail.com>
2019-04-12 16:45:15 +0200

    final instructions and basic activities

--- Day 1 - Kotlin Basics/DoggoApp/app/src/main/java/com/womenwhocode/workshop/doggoapp/MainActivity.kt
+++ Day 1 - Kotlin Basics/DoggoApp/app/src/main/java/com/womenwhocode/workshop/doggoapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.womenwhocode.workshop.doggoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * View HugeDogActivity
+     */
+    fun viewHugeDog() {
+        startActivity(Intent(this, HugeDogActivity::class.java))
     }
 }
